722795cf95d91bd7c25c484de1c530cf80375ae9
mercurystraw <lzhseu2023@163.com>
2024-12-06 10:57:02 +0800

    final labwork 6

--- labwork6/wavelet.py
+++ labwork6/wavelet.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import pywt
-
 # 读取TIFF格式的图像
 image = cv2.imread('./resources_labwork6/sinePulses.tif', cv2.IMREAD_GRAYSCALE)
 
@@ -30,13 +29,36 @@ def wavelet(image, wavelet, threshold=20):
     return np.clip(denoised_image, 0, 255).astype(np.uint8)
 
 
+def wavelet_edge_detection(image, wavelet, level=3):
+    # 将图像转换为浮点数类型
+    image = image.astype(np.float32) / 255.0
+
+    # 小波分解
+    coeffs = pywt.wavedec2(image, wavelet, level=level)
+
+    # 提取近似分量和细节系数（水平、垂直、对角线）
+    approx_coeff = coeffs[0]
+    detail_coeffs = coeffs[1:]
+
+    # 消除近似分量（将其设为零）
+    approx_coeff *= 0
+
+    # 重建图像（反变换）
+    reconstructed_image = pywt.waverec2([approx_coeff] + detail_coeffs, wavelet)
+
+    # 将重建图像转换为 uint8 数据类型
+    edge_image = np.clip(reconstructed_image * 255, 0, 255).astype(np.uint8)
+
+    return edge_image
+
+
 # 不同的小波函数
 wavelets = ['haar', 'db1', 'db2', 'sym2', 'coif1']
 for wavelet_type in wavelets:
     denoised_image = wavelet(noisy_image, wavelet_type)
     cv2.imwrite('./resources_labwork6/denoised_image_' + wavelet_type + '.tif', denoised_image)
-    # Canny边缘检测
-    edges = cv2.Canny(denoised_image, 100, 200)
+
+    edges = wavelet_edge_detection(denoised_image, wavelet_type)
     cv2.imwrite('./resources_labwork6/edges_' + wavelet_type + '.tif', edges)
 
 print('图片处理完成！')
